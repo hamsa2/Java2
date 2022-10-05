8cc4a918a2132352f0bc078f6b8ad11be9365783
Nailiya <66hamsa@gmail.com>
2022-10-05 13:08:56 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 8000;
+        int bonus = 20;
+        int bonusMiles = amount / bonus;
+        System.out.println("Бонусные мили: " + bonusMiles);
+    }
+}
